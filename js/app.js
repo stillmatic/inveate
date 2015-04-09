f98d67a00c0800e6926ee4ff7134d9a94e8e805d
Chris Hua <hua.christopher@gmail.com>
2015-04-09 19:25:09 -0400

    barebones
    markdown + initial theming + navbar

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var navBar = React.createClass({ 
+	render: function() {
+		return <div class="navBar"> <ul> <li> Home </li> <li> Home </li> <li> Home </li> <li> Home </li> </ul> </div>;
+	});
+});
+
+React.render(<navBar />, navBar);
