9379dec4584f072d01c66fec1ebf32c0a4220626
Chris Hua <hua.christopher@gmail.com>
2015-04-14 00:34:09 -0400

    team grids + 404
    * properly aligns team members in grid
    * begin styling
    * add masonry deps
    * 404.jade added

--- README.md
+++ README.md
@@ -3,12 +3,20 @@
 chua@wharton.upenn.edu
 
 static site built with harp. simple 'agency' style website served from json and md files. 
+in the (distant) future i'd like to move off of harp; should be reasonably easy to call terraform natively to do the preprocessing that we really need it to
 
 * should be easily editable by programmming novices
 * should have a nice look and 'just work'
+* minimize external dependencies, harp has enough built in 
 
-	npm install -g harp
-	local:
-	harp server --port 9000
-	harp compile`
+```
+npm install -g harp
+cd inv
+harp server --port 9000
+```
 
+to compile and deploy
+```
+harp compile
+```
+(rsync stuff goes here)
--- inv/app.js
+++ inv/app.js
@@ -1,6 +1,11 @@
 var $container = $('#people-box');
-var transform = {'tag':'figure', 'class' : 'people', 'html': '<img src="${URL}"> <figcaption> ${Name} </figcaption>'}
+var transform = {
+    'tag': 'figure',
+    'class': 'people',
+    'html': '<img src="${URL}"> <figcaption> <strong>${Name}</strong> <br /> <strong>Category:</strong> ${Category} <br /> <strong>Email:</strong> ${Email} <br /> <strong>Bio:</strong> ${Bio} </figcaption>'
+}
 
+//TODO: ajax this
 var people = [{
     "Name": "Chris Hua",
     "Category": "Web Development",
@@ -14,13 +19,42 @@ var people = [{
     "Email": "",
     "Bio": "Thom & YUUUU",
     "URL": "http://placehold.it/250x250"
+}, {
+    "Name": "Sam Summer",
+    "Category": "Web Development",
+    "Email": "chua@wharton.upenn.edu",
+    "Bio": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    "Twitter": "@chris_hua",
+    "URL": "http://placehold.it/250x250"
+}, {
+    "Name": "Anvita Anchar",
+    "Category": "Mechanical Design",
+    "Email": "",
+    "Bio": "lorem",
+    "URL": "http://placehold.it/250x250"
+}, {
+    "Name": "Anvita Anchar",
+    "Category": "Mechanical Design",
+    "Email": "",
+    "Bio": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio vitae, error cum quasi odit a est, ullam facilis debitis in animi porro ad molestias non dolorum quod eos. Veritatis, dignissimos.",
+    "URL": "http://placehold.it/250x250"
 }]
 
-$container.append(json2html.transform(people, transform));
+document.getElementById('people-box').innerHTML = json2html.transform(people, transform);
+
+// make this declarative instead of functional
+// necessary because j2h doesn't change height of div
+function resize() {
+    this._originalHeight = this._originalHeight || $(this).height();
+    var bodyWidth = $("body").width();
+    var widthDiff = bodyWidth - originalWidth;
+    var newHeight = this._originalHeight + (widthDiff / 10);
+    $(this).css("height", newHeight);
+}
+
+$.resize();
 
 $container.isotope({
     itemSelector: '.people',
-    masonry: {
-        columnWidth: 250
-    }
+    layoutMode: 'masonryHorizontal'
 });
